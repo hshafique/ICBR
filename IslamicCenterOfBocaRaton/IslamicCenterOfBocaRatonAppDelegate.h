1caf77627390380b9d45dea52815a925891ba01e
Hashim Shafique <hashimshafique@Hashim-Shafiques-MacBook-Pro.local>
2011-03-18 22:46:31 -0400

    Initial Commit

---
+++ IslamicCenterOfBocaRaton/IslamicCenterOfBocaRatonAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  IslamicCenterOfBocaRatonAppDelegate.h
+//  IslamicCenterOfBocaRaton
+//
+//  Created by Hashim Shafique on 3/18/11.
+//  Copyright 2011 Siemens Enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IslamicCenterOfBocaRatonAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ IslamicCenterOfBocaRaton/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IslamicCenterOfBocaRaton
+//
+//  Created by Hashim Shafique on 3/18/11.
+//  Copyright 2011 Siemens Enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
